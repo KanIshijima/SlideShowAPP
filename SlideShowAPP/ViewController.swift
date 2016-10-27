6f120da7feb5efd4a0d2aadc1015446fabeb9cea
aozora <siromon@ezweb.ne.jp>
2016-10-27 22:00:03 +0900

    10.27

--- SlideShowAPP/ViewController.swift
+++ SlideShowAPP/ViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    
      let photos = ["img_001.jpg", "img_002.jpg", "img_003.jpg", "img_004.jpg"]
     var index = 0
     //上記はindexの変数は初めは０と定義
+    var timer = NSTimer()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,45 +29,57 @@ class ViewController: UIViewController {
         //上記2行は写真をimageとして扱い、ナンバーを０として扱うという意味
             
         imageView.userInteractionEnabled = true
-        ImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "imageTapped:"))
-        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController())))
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.imageTapped(_:))))
         
-        //ラベルを作る.
-        myLabel = UILabel(frame: CGRectMake(0,0,200,50))
-        myLabel.backgroundColor = UIColor.orangeColor()
-        myLabel.layer.masksToBounds = true
-        myLabel.layer.cornerRadius = 20.0
-        myLabel.text = "Time:\(cnt)"
-        myLabel.textColor = UIColor.whiteColor()
-        myLabel.shadowColor = UIColor.grayColor()
-        myLabel.textAlignment = NSTextAlignment.Center
-        myLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 200)
-        self.view.backgroundColor = UIColor.cyanColor()
-        self.view.addSubview(myLabel)
+      
 
-        //タイマーを作る.
-        NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(ViewController.onUpdate(_:)), userInfo: nil, repeats: true)
-        
-        func next(sender: AnyObject) {
-            index += 1
-            //上記はindexを１づつアップさせる（次のボタンのアクション）
-            if index == 4{
-                index = 0
-            }
+          }
+    @IBAction func next(sender: AnyObject) {
+        index += 1
+        //上記はindexを１づつアップさせる（次のボタンのアクション）
+        if index == 4{
+            index = 0
         }
-        func back(sender: AnyObject) {
-                
-                index -= 1
-                //上記はindexを１づつダウンさせる（戻るボタンのアクション）
-                if index == 0{
-                    index = 4
-                }
-        }
-    
         
         let image = UIImage(named:photos[index])
         imageView.image = image
         //上記２行は
+        
+    }
+    
+    @IBAction func back(sender: AnyObject) {
+        
+        index -= 1
+        //上記はindexを１づつダウンさせる（戻るボタンのアクション）
+        if index < 0{
+            index = 3
+        }
+        let image = UIImage(named:photos[index])
+        imageView.image = image
+        //上記２行は
+
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        if timer.valid == true{
+            timer.invalidate()
+            playButton.setTitle("再生", forState: .Normal)
+            nextButton.enabled = true
+            backButton.enabled = true
+            
+        }else{
+            timer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: #selector(ViewController.onUpdate(_:)), userInfo: nil, repeats: true)
+            playButton.setTitle("停止", forState: .Normal)
+            nextButton.enabled = false
+            backButton.enabled = false
+        }
+        
+
+
+    }
+
+    
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -71,23 +89,19 @@ class ViewController: UIViewController {
     
     func imageTapped(sender: UITapGestureRecognizer) {
         print(sender)
+        
+        presentViewController(<#T##viewControllerToPresent: UIViewController##UIViewController#>, animated: <#T##Bool#>, completion: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
     }
     
     //この後タイマー　時間計測用の変数.
     var cnt : Float = 0
     
-    //時間表示用のラベル.
-    var myLabel : UILabel!
     
     //NSTimerIntervalで指定された秒数毎に呼び出されるメソッド.
     func onUpdate(timer : NSTimer){
         
-        cnt += 0.1
-        
-        //桁数を指定して文字列を作る.
-        let str = "Time:".stringByAppendingFormat("%.1f",cnt)
-        
-        myLabel.text = str
+       next(timer)
+    }
     
 //
 //        var barHeight: CGFloat = 0.0
